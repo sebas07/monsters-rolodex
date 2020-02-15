34b9487b3acc5c5b3a4d3294e66f82d175d01093
Sebastian Rodriguez <josesrb10@outlook.com>
2020-02-14 20:30:02 -0600

    Updates

---
+++ precache-manifest.72e5328d72aa040b54dfa58ff1ee8982.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af0d6152952d558b80fffe5db0639745",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "667bb863abac85599269",
+    "url": "/monsters-rolodex/static/css/main.74c11dc6.chunk.css"
+  },
+  {
+    "revision": "6232f7938c51629e79e8",
+    "url": "/monsters-rolodex/static/js/2.c1150fa6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.c1150fa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "667bb863abac85599269",
+    "url": "/monsters-rolodex/static/js/main.24f11732.chunk.js"
+  },
+  {
+    "revision": "60b3d0d6b00356a50eb0",
+    "url": "/monsters-rolodex/static/js/runtime-main.916b3b7d.js"
+  }
+]);
